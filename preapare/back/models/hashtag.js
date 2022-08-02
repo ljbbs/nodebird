d107d1e667ee94ad51d37168328a44e7c87086ee
Jeongbeom Lee <ljbbs@naver.com>
2022-08-03 00:02:45 +0900

    중간 커밋

--- preapare/back/models/hashtag.js
+++ preapare/back/models/hashtag.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
     collate: 'utf8mb4_general_ci',
   });
   Hashtag.associate = (db) => {
-    db.Hashtag.belongsToMany(db.Post);
+    db.Hashtag.belongsToMany(db.Post, { through: 'PostHashtag' });
   };
   return Hashtag;
 };
