92fabc3f130fc604b72b1c58895eda45b1cde062
Jeongbeom Lee <ljbbs@naver.com>
2022-11-30 15:35:24 +0900

    중간 커밋

--- preapare/front/next.config.js
+++ preapare/front/next.config.js
@@ -1,6 +1,25 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === 'production';
+    const plugins = [
+      ...config.plugins,
+      // new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+    ];
+
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'eval',
+      plugins,
+    };
+  },
+  compress: true,
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
+});
